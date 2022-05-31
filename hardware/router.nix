ee8011a5960b7c5932c61170f34eb7139bf20242
Alexander Averyanov <alex@averyan.ru>
2022-05-31 23:40:12 +0300

    uPDATE UUID

--- hardware/router.nix
+++ hardware/router.nix
@@ -17,24 +17,16 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/da37bb93-6f50-4dc8-afbc-a52076a4200b";
+    device = "/dev/disk/by-uuid/6f802dce-a57f-4363-8e15-ea2f1c3cfa90";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/63A0-3DA4";
+    device = "/dev/disk/by-uuid/C739-BFD6";
     fsType = "vfat";
   };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp6s18.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.intel.updateMicrocode = false;
 }
