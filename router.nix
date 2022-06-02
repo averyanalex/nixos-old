ce96618d3d89ca184ff6698038d571c2028fef22
Alexander Averyanov <alex@averyan.ru>
2022-06-02 19:32:12 +0300

    Yggdrasil peers

--- router.nix
+++ router.nix
@@ -35,6 +35,12 @@
     persistentKeys = true;
     config = {
       Listen = [ "tcp://0.0.0.0:5353" ];
+      Peers = [
+        "tls://ygg.loskiq.ru:17314"
+        "tls://kazi.peer.cofob.ru:18001"
+        "tls://yggno.de:18227"
+        "tls://box.paulll.cc:13338"
+      ];
       IfName = "ygg0";
     };
   };
