47f92303cd606c0d9365f2989d34ac67d387f790
Alexander Averyanov <alex@averyan.ru>
2022-06-04 18:06:38 +0300

    ht mounts

--- mounts/highterum.nix
+++ mounts/highterum.nix
@@ -2,13 +2,13 @@
 
 {
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/6f802dce-a57f-4363-8e15-ea2f1c3cfa90";
+    device = "/dev/disk/by-uuid/f93819ee-d7d3-49b3-a918-98bfa04637f1";
     fsType = "ext4";
     options = [ "discard" ];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C739-BFD6";
+    device = "/dev/disk/by-uuid/453A-5406";
     fsType = "vfat";
   };
 }
