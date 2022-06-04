1c9f765b71ca8cb4f88d0f94ee4b638595557842
Alexander Averyanov <alex@averyan.ru>
2022-06-04 18:11:18 +0300

    ssh comment change

--- common.nix
+++ common.nix
@@ -51,7 +51,7 @@
       uid = 1000;
       passwordFile = config.age.secrets.password-alex.path;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF5KDy1T6Z+RlDb+Io3g1uSZ46rhBxhNE39YlG3GPFM averyanalex@gmail.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF5KDy1T6Z+RlDb+Io3g1uSZ46rhBxhNE39YlG3GPFM alex@averyan.ru"
       ];
     };
   };
