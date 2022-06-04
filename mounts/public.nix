8a22fc440568a420c0f785b8a1c05042beed3fc4
Alexander Averyanov <alex@averyan.ru>
2022-06-04 13:01:33 +0300

    discard

--- mounts/public.nix
+++ mounts/public.nix
@@ -4,6 +4,7 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/7c0b2781-e79b-42bb-892e-a55162147816";
     fsType = "ext4";
+    options = [ "discard" ];
   };
 
   fileSystems."/boot" = {
--- mounts/router.nix
+++ mounts/router.nix
@@ -4,6 +4,7 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/6f802dce-a57f-4363-8e15-ea2f1c3cfa90";
     fsType = "ext4";
+    options = [ "discard" ];
   };
 
   fileSystems."/boot" = {
