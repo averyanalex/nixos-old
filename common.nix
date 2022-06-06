ccc258f39c478350a869c6cbadffa71c30d0a4b0
Alexander Averyanov <alex@averyan.ru>
2022-06-06 14:38:56 +0300

    install screen

--- common.nix
+++ common.nix
@@ -33,7 +33,7 @@
     };
   };
 
-  environment.systemPackages = [ pkgs.htop pkgs.ncdu pkgs.micro ];
+  environment.systemPackages = [ pkgs.htop pkgs.ncdu pkgs.micro pkgs.screen ];
   environment.shellAliases = {
     nixupd = ''
       sudo rm -rf /root/.cache && sudo nixos-rebuild switch --flake "github:averyanalex/nixos"'';
