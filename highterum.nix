02f73258c20e53393004dd9156fa4ae38a84749f
Alexander Averyanov <alex@averyan.ru>
2022-06-11 20:12:12 +0300

    disable cringe

--- highterum.nix
+++ highterum.nix
@@ -27,45 +27,6 @@
     owner = "alex";
   };
 
-  virtualisation.oci-containers.backend = "docker";
-  virtualisation.oci-containers.containers = {
-    ht-pgsql = {
-      image = "postgres:14";
-      ports = [ "5432:5432" ];
-      volumes = [ "/var/lib/ht-pgsql:/var/lib/postgresql/data" ];
-      environmentFiles = [ config.age.secrets.highterum-pgsql.path ];
-    };
-    ht-cabinet = {
-      image = "cr.averyan.ru/highterum/simple-cabinet/web-api";
-      ports = [ "8085:8080" ];
-      volumes = [
-        "/var/lib/ht-cabinet:/app/data"
-        "${config.age.secrets.ht-cabinet-api.path}:/app/data/application.properties"
-      ];
-    };
-    ht-cabinet-frontend = {
-      image = "cr.averyan.ru/highterum/simple-cabinet/frontend-2";
-      ports = [ "8086:80" ];
-    };
-  };
-
-  services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    recommendedOptimisation = true;
-    clientMaxBodySize = "8g";
-    virtualHosts = {
-      "assets" = {
-        serverName = "192.168.44.2:8087";
-        listen = [{
-          port = 8087;
-          addr = "192.168.44.2";
-        }];
-        locations."/".root = "/var/lib/ht-cabinet/assets";
-      };
-    };
-  };
-
   networking = {
     hostName = "highterum";
 
