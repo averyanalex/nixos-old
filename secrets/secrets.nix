02464ea8c2d870a5bb09164cecb1a7fa4a838615
Alexander Averyanov <alex@averyan.ru>
2022-06-16 23:53:33 +0300

    rekey mole

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,8 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF5KDy1T6Z+RlDb+Io3g1uSZ46rhBxhNE39YlG3GPFM";
   users = [ alex ];
 
+  mole =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2cUpFVFLAXnAq9wtc5DXYjUfjnegSmW8TpBjN+PrkA";
   router =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoe/I5CQ++Vr+4EXURrh3dphT/wGbCWSClu7FOB8zbs";
   public =
@@ -11,7 +13,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEt5lXWlcNxeS/dZtKiJDyzHXnLlVFUe9iC0LPHPly6X";
   highterum =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBioDhlj6H4xSK8K2sr9uxz1g9y6OD87mNrpQX0NL9OW";
-  systems = [ router public runner highterum ];
+  systems = [ mole router public runner highterum ];
 in {
   "passwords/alex.age".publicKeys = users ++ systems;
 
