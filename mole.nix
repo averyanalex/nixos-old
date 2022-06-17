c51d2b2d5329c666e3454197e1ef2bee4e05626c
Alexander Averyanov <alex@averyan.ru>
2022-06-17 12:08:34 +0300

    Uptime Kuma

--- mole.nix
+++ mole.nix
@@ -4,6 +4,14 @@
   imports =
     [ ./hardware/aeza.nix ./mounts/mole.nix ./common.nix ./lang/en.nix ];
 
+  virtualisation.oci-containers.containers = {
+    hackagecompare = {
+      image = "louislam/uptime-kuma:1";
+      ports = [ "3001:3001" ];
+      volumes = [ "/var/lib/uptime-kuma:/app/data" ];
+    };
+  };
+
   boot.kernel.sysctl = {
     # By default, not automatically configure any IPv6 addresses.
     "net.ipv6.conf.all.accept_ra" = 0;
