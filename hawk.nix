f9e9ed4109dea5fa0eda37de31e38dbc4e650207
Alexander Averyanov <alex@averyan.ru>
2022-06-20 16:41:46 +0300

    log

--- hawk.nix
+++ hawk.nix
@@ -139,7 +139,7 @@
             ct state { established, related } counter accept comment "accept traffic originated from us"
 
             # count and drop any other traffic
-            counter drop
+            counter log drop
           }
 
           chain forward {
