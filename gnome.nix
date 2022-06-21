f79e3ab975773b19b4173a7d58985ec856331875
Alexander Averyanov <alex@averyan.ru>
2022-06-21 14:15:05 +0300

    clear gnome

--- gnome.nix
+++ gnome.nix
@@ -8,4 +8,13 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages = (with pkgs; [ gnome-tour ])
+    ++ (with pkgs.gnome; [
+      epiphany # web browser
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+    ]);
 }
