6e07decba0bc10d05251c5f64396405ce919e3e9
Alexander Averyanov <alex@averyan.ru>
2022-06-29 23:36:57 +0300

    Ашч

--- modules/docker.nix
+++ modules/docker.nix
@@ -9,26 +9,34 @@
     '';
   };
 
-  config = lib.mkIf config.virtualisation.docker.enable {
-    # auto cleanup
-    virtualisation.docker.autoPrune.enable = true;
-    virtualisation.docker.autoPrune.dates = "daily";
-    virtualisation.docker.autoPrune.flags = [ "--all" ];
+  config = lib.mkMerge [
+    (lib.mkIf config.virtualisation.docker.enable {
+      # auto cleanup
+      virtualisation.docker.autoPrune.enable = true;
+      virtualisation.docker.autoPrune.dates = "daily";
+      virtualisation.docker.autoPrune.flags = [ "--all" ];
 
-    # access docker from normal user
-    users.users.alex.extraGroups = [ "docker" ];
+      # access docker from normal user
+      users.users.alex.extraGroups = [ "docker" ];
 
-    # docker-compose
-    environment.systemPackages = [ pkgs.docker-compose_2 ];
-    environment.shellAliases = { dc = "docker compose"; };
-
-    # watchtower
-    virtualisation.oci-containers.containers =
-      lib.Mkif config.virtualisation.docker.enableWatchtower {
-        watchtower = {
+      # docker-compose
+      environment.systemPackages = [ pkgs.docker-compose_2 ];
+      environment.shellAliases = { dc = "docker compose"; };
+    })
+    (lib.mkIf config.virtualisation.docker.enableWatchtower {
+      virtualisation.oci-containers = {
+        backend = "docker";
+        containers.watchtower = {
           image = "containrrr/watchtower";
           volumes = [ "/var/run/docker.sock:/var/run/docker.sock" ];
         };
       };
-  };
+    })
+    {
+      assertions = [{
+        assertion = config.virtualisation.docker.enableWatchtower;
+        message = "ID must be 1-999";
+      }];
+    }
+  ];
 }
