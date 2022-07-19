120b1b7af04a893d31a944be15d31e0ce8234165
Alexander Averyanov <alex@averyan.ru>
2022-07-19 23:02:50 +0300

    Gaps

--- home/alex-alligator.nix
+++ home/alex-alligator.nix
@@ -41,6 +41,22 @@
       menu = "${pkgs.wofi}/bin/wofi -c ~/.config/wofi/config -I";
       modifier = "Mod4"; # Super
     };
+    extraConfig = ''
+      gaps inner 5
+      gaps outer 5
+      default_border pixel 1
+      smart_borders on
+
+      for_window [shell="xdg_shell"] title_format "%title (%app_id)"
+      for_window [shell="x_wayland"] title_format "%class - %title"
+
+      ## Window decoration
+      # class                 border  backgr. text    indicator child_border
+      client.focused          #88c0d0 #434c5e #eceff4 #8fbcbb   #88c0d0
+      client.focused_inactive #88c0d0 #2e3440 #d8dee9 #4c566a   #4c566a
+      client.unfocused        #88c0d0 #2e3440 #d8dee9 #4c566a   #4c566a
+      client.urgent           #ebcb8b #ebcb8b #2e3440 #8fbcbb   #ebcb8b
+    '';
   };
 
   xdg.configFile."wofi/config".source = ./configs/wofi/config;
