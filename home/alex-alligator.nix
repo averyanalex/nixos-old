fa245af4ae4e429fe5db436687730cf2a37dbcb4
Alexander Averyanov <alex@averyan.ru>
2022-07-19 23:21:30 +0300

    History

--- home/alex-alligator.nix
+++ home/alex-alligator.nix
@@ -38,6 +38,9 @@
       #     xkb_options = "grp:alt_shift_toggle";
       #   };
       # };
+      keybindings = {
+        "${cfg.config.modifier}+h" = "exec clipman pick -t wofi";
+      };
       output = {
         "DP-1" = { mode = "3440x1440@144Hz"; };
       };
