a3f9e25735739375a12106802a4f2041a2321cf3
Alexander Averyanov <alex@averyan.ru>
2022-07-19 20:58:19 +0300

    enable opengl

--- hardware/amdgpu.nix
+++ hardware/amdgpu.nix
@@ -3,14 +3,16 @@
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
 
-  hardware.opengl.extraPackages = with pkgs; [
-    rocm-opencl-icd
-    rocm-opencl-runtime
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      rocm-opencl-icd
+      rocm-opencl-runtime
 
-    amdvlk
-    # driversi686Linux.amdvlk
-  ];
-
-  hardware.opengl.driSupport = true;
-  # hardware.opengl.driSupport32Bit = true;
+      amdvlk
+      # driversi686Linux.amdvlk
+    ];
+  };
 }
