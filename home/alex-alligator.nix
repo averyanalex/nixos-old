dadd92397f0c27986a3595482e4326a43b417a2e
Alexander Averyanov <alex@averyan.ru>
2022-07-20 11:26:05 +0300

    Autostart TG

--- home/alex-alligator.nix
+++ home/alex-alligator.nix
@@ -65,8 +65,11 @@
       exec clipman restore
       exec wl-paste -t text --watch clipman store
 
-      exec ${pkgs.mako}/bin/mako # desktop notifications
-      # exec gnome-keyring-daemon --daemonize --start --components=gpg,pkcs11,secrets,ssh # secret service
+      # desktop notifications
+      exec ${pkgs.mako}/bin/mako
+      
+      # secret service
+      # exec gnome-keyring-daemon --daemonize --start --components=gpg,pkcs11,secrets,ssh
 
       # STYLING
       gaps inner 5
@@ -77,16 +80,16 @@
       for_window [shell="xdg_shell"] title_format "%title (%app_id)"
       for_window [shell="x_wayland"] title_format "%class - %title"
 
-      ## Window decoration
+      ## window decoration
       # class                 border  backgr. text    indicator child_border
       client.focused          #88c0d0 #434c5e #eceff4 #8fbcbb   #88c0d0
       client.focused_inactive #88c0d0 #2e3440 #d8dee9 #4c566a   #4c566a
       client.unfocused        #88c0d0 #2e3440 #d8dee9 #4c566a   #4c566a
       client.urgent           #ebcb8b #ebcb8b #2e3440 #8fbcbb   #ebcb8b
+
+      # AUTOSTART
+      exec telegram-desktop -startintray
     '';
-    # extraSessionCommands = ''
-    #   eval $(gnome-keyring-daemon --start)
-    # '';
   };
 
   services.gnome-keyring.enable = true;
