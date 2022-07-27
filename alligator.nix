2afbf107635ed121adf75152eff5082ee1432d08
averyanalex <alex@averyan.ru>
2022-07-27 07:00:46 +0300

    Swaysupport.nix

--- alligator.nix
+++ alligator.nix
@@ -7,26 +7,16 @@
     ./common.nix
     ./lang/ru.nix
 
-    ./pipewire.nix
+    ./swaysupport.nix
   ];
 
   boot.kernelPackages = pkgs.unstable.linuxKernel.packages.linux_xanmod_tt;
-
-  services.getty.autologinUser = "alex";
 
   environment.systemPackages = with pkgs; [
     lm_sensors
   ];
 
   virtualisation.docker.enable = true;
-
-  services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-    gtkUsePortal = true;
-  };
 
   services.yggdrasil = {
     enable = true;
@@ -37,28 +27,6 @@
       ];
       IfName = "ygg0";
     };
-  };
-
-  fonts.enableDefaultFonts = true;
-  programs.dconf.enable = true;
-
-  users.users.alex.extraGroups = [ "video" ];
-  programs.light.enable = true;
-
-  age.secrets.wayvnc = {
-    file = ./secrets/wayvnc.age;
-    path = "/home/alex/.config/wayvnc/config";
-    owner = "alex";
-  };
-  age.secrets.wayvnc-cert = {
-    file = ./secrets/wayvnc-cert.age;
-    path = "/home/alex/.config/wayvnc/cert.pem";
-    owner = "alex";
-  };
-  age.secrets.wayvnc-key = {
-    file = ./secrets/wayvnc-key.age;
-    path = "/home/alex/.config/wayvnc/key.pem";
-    owner = "alex";
   };
 
   networking = {
---
+++ swaysupport.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./pipewire.nix ];
+  services.getty.autologinUser = "alex";
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+  };
+
+  fonts.enableDefaultFonts = true;
+  programs.dconf.enable = true;
+
+  users.users.alex.extraGroups = [ "video" ];
+  programs.light.enable = true;
+
+  age.secrets.wayvnc = {
+    file = ./secrets/wayvnc.age;
+    path = "/home/alex/.config/wayvnc/config";
+    owner = "alex";
+  };
+  age.secrets.wayvnc-cert = {
+    file = ./secrets/wayvnc-cert.age;
+    path = "/home/alex/.config/wayvnc/cert.pem";
+    owner = "alex";
+  };
+  age.secrets.wayvnc-key = {
+    file = ./secrets/wayvnc-key.age;
+    path = "/home/alex/.config/wayvnc/key.pem";
+    owner = "alex";
+  };
+}
