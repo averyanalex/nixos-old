69afef488bd15df65e8c95ce0ce6b61987f8cf21
averyanalex <alex@averyan.ru>
2022-07-28 19:26:25 +0300

    Extensions

--- home/dev.nix
+++ home/dev.nix
@@ -13,7 +13,29 @@
     extensions = with pkgs.unstable.vscode-extensions; [
       serayuzgur.crates
       jnoortheen.nix-ide
+      bradlc.vscode-tailwindcss
+      editorconfig.editorconfig
+      dbaeumer.vscode-eslint
+      esbenp.prettier-vscode
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "svelte-vscode";
+        publisher = "svelte";
+        version = "105.20.0";
+        sha256 = "+vYNgKVuknPROKTMMHugc9VrvYZ7GONr5SgYsb7l5rs=";
+      }
+      {
+        name = "vscode-todo-highlight";
+        publisher = "wayou";
+        version = "1.0.5";
+        sha256 = "CQVtMdt/fZcNIbH/KybJixnLqCsz5iF1U0k+GfL65Ok=";
+      }
+      {
+        name = "gitlab-workflow";
+        publisher = "GitLab";
+        version = "3.47.2";
+        sha256 = "VeL3yvfaNIHUPaZHDxSE8pbdh4c93uMjhSMv2PWR6ts=";
+      }
       {
         name = "even-better-toml";
         publisher = "tamasfe";
@@ -35,6 +57,7 @@
     ];
     userSettings = {
       "nix.enableLanguageServer" = true;
+      "svelte.enable-ts-plugin" = true;
       "rust-analyzer.server.path" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
 
       "files.autoSave" = "afterDelay";
