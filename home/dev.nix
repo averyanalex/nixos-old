ef80a039c2a27aaed08820d4ae37157c76cc1e0e
averyanalex <alex@averyan.ru>
2022-07-28 23:19:53 +0300

    Git sommit signing

--- home/dev.nix
+++ home/dev.nix
@@ -81,6 +81,10 @@
     enable = true;
     userName = "averyanalex";
     userEmail = "alex@averyan.ru";
+    signing = {
+      signByDefault = true;
+      key = "6AF4D168E4B6C49A";
+    };
     extraConfig = {
       init.defaultBranch = "main";
     };
