3d683b975cc418b7a4dd9afc45fd6cf71a2cf0b7
averyanalex <alex@averyan.ru>
2022-07-28 14:11:13 +0300

    VSCode direnv

--- home/dev.nix
+++ home/dev.nix
@@ -21,11 +21,25 @@
         version = "0.16.5";
         sha256 = "pRUiXsZGhTIQx2Qx9NFQ7OGRros3KdzjUlq13nm4pAc=";
       }
+      # {
+      #   name = "nix-env-selector";
+      #   publisher = "arrterian";
+      #   version = "1.0.7";
+      #   sha256 = "DnaIXJ27bcpOrIp1hm7DcrlIzGSjo4RTJ9fD72ukKlc=";
+      # }
+      {
+        name = "direnv";
+        publisher = "mkhl";
+        version = "0.6.1";
+        sha256 = "5/Tqpn/7byl+z2ATflgKV1+rhdqj+XMEZNbGwDmGwLQ=";
+      }
     ];
     userSettings = {
       "nix.enableLanguageServer" = true;
+
       "files.autoSave" = "afterDelay";
       "diffEditor.ignoreTrimWhitespace" = false;
+
       "git.confirmSync" = false;
       "git.enableSmartCommit" = true;
       "git.autofetch" = true;
