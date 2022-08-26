e5b314ed9a8ace5977b66696004ba8224162a4cf
AveryanAlex <alex@averyan.ru>
2022-08-26 16:29:26 +0300

    Music sync

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,4 +1,7 @@
 { lib, config, pkgs, ... }:
+let
+  allDevices = [ "alligator" ];
+in
 {
   config.services.syncthing = lib.mkIf config.services.syncthing.enable {
     user = "alex";
@@ -13,6 +16,12 @@
         id = "RZP5BDL-HVPSCK7-OETCWX7-DLBF33F-D2TFH7K-3JFC6IK-PNJQQGO-2PN44QA";
       };
       hamster.id = "VH7MYCJ-CG6L4IO-M3YSBMF-67S5RVM-PG4MPCH-7K3PNMC-UQPMHPF-RKDWCAN";
+    };
+    folders = {
+      "/home/alex/Music" = {
+        id = "music";
+        devices = allDevices;
+      };
     };
   };
 }
