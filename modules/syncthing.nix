395f0453244db00d1624fbc182e84af51b00541d
AveryanAlex <alex@averyan.ru>
2022-08-27 19:25:43 +0300

    PocoFT ST

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,6 +1,6 @@
 { lib, config, pkgs, ... }:
 let
-  allDevices = [ "alligator" "hamster" ];
+  allDevices = [ "alligator" "hamster" "pocoft" ];
 in
 {
   config.services.syncthing = lib.mkIf config.services.syncthing.enable {
@@ -16,6 +16,7 @@ in
         id = "RZP5BDL-HVPSCK7-OETCWX7-DLBF33F-D2TFH7K-3JFC6IK-PNJQQGO-2PN44QA";
       };
       hamster.id = "VH7MYCJ-CG6L4IO-M3YSBMF-67S5RVM-PG4MPCH-7K3PNMC-UQPMHPF-RKDWCAN";
+      pocoft.id = "4YTG43C-6RYBYZV-V6P24VC-MQWYOBX-QO2FPYC-4AYR3TJ-DQDXZX4-BVPO2AO";
     };
     folders = {
       "/home/alex/Music" = {
